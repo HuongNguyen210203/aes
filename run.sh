a7a3c0656f09e25feb94e0a09545707704b53d7c
debian <debian@debians-MacBook-Pro.local>
2025-05-08 23:16:52 +0700

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+#ensure /bin exist
+mkdir ./bin
+
+#compile
 g++ -std=c++11  -o ./bin/aes ./src/*.cpp ./main.cpp
 
-# clear
+#format
 echo
+echo "###########This is the program part(non-part program):"
 echo
-echo "###########This is the program part:"
-echo
+
+#run bin
 ./bin/aes
